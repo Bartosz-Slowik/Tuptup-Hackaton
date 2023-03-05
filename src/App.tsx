8535b7dc074e2a041e15dc23fd152ac4d942a125
SzymonSalamon <92257198+SzymonSalamon@users.noreply.github.com>
2023-03-05 21:33:17 +0100

    add_event button in progress

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 import './App.css';
 import Navi from "./components/Navi/navi";
 import Map from "./components/Map/Map";
-
+import Add_event from './components/Add_event/add_event';
 function App() {
   return (
 
     <div className="w-screen flex flex-col">
       <Map />
       <Navi />
-      
+      <Add_event />
     </div>
     
   );
