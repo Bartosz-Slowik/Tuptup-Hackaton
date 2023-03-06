2f74d9d61c2a4fff1a7ac748ac7b63992c28ad82
Adrian Szlag <92638498+AdrianSzlag@users.noreply.github.com>
2023-03-06 00:28:43 +0000

    more ui

--- src/components/events/events.tsx
+++ src/components/events/events.tsx
@@ -8,16 +8,20 @@ import { MdEmojiPeople } from "react-icons/md";
 import { BiCalendarEvent } from "react-icons/bi";
 import BigButton from "./bigButton";
 import Event from "./event";
+import places from "../places.json";
+
+const eventTypes = ["sport", "party", "event", "meeting"];
 
 export default function Events() {
   const [fullScreen, setFullScreen] = useState(false);
+  const [filter, setFilter] = useState<"friends" | "events" | "all">("all");
 
   return (
     <div
       className={`${
-        fullScreen && "!h-full !max-h-none !rounded-none !z-30"
-      } fixed bottom-0 left-0 right-0 bg-white z-10
-        p-2 rounded-t-3xl shadow-3xl h-[20rem] transition-all duration-500
+        fullScreen && " !bottom-0 !rounded-none !z-30"
+      } fixed flex flex-col -bottom-[20rem] left-0 right-0 bg-white z-10
+        p-2 rounded-t-3xl shadow-3xl h-full transition-all duration-500
         md:top-0 md:h-full md:transition-none md:rounded-none md:right-auto md:w-[400px]`}
     >
       <div
@@ -50,26 +54,76 @@ export default function Events() {
           } h-4 w-4 transition-all duration-500`}
         />
       </div>
-      <div className="flex flex-col p-2">
-        <div className="flex flex-row space-x-2">
-          <BigButton Icon={CameraIcon} text="Take a photo" onClick={() => {}} />
-          <BigButton
-            Icon={BiCalendarEvent}
-            text="Create event"
-            onClick={() => {}}
-          />
+      <div
+        className={`${
+          fullScreen && "flex-col-reverse"
+        } flex flex-col md:flex-col-reverse flex-grow`}
+      >
+        <div className="flex flex-col p-2">
+          <div className="flex flex-row space-x-2">
+            <BigButton
+              Icon={CameraIcon}
+              text="Take a photo"
+              onClick={() => {}}
+            />
+            <BigButton
+              Icon={BiCalendarEvent}
+              text="Create event"
+              onClick={() => {}}
+            />
+          </div>
+          <div className="flex flex-row space-x-2 mt-2">
+            <BigButton
+              Icon={MdEmojiPeople}
+              text="Friends"
+              onClick={() => {
+                filter === "friends" ? setFilter("all") : setFilter("friends");
+              }}
+              isSelected={filter === "friends"}
+            />
+            <BigButton
+              Icon={StarIcon}
+              text="Events"
+              onClick={() => {
+                filter === "events" ? setFilter("all") : setFilter("events");
+              }}
+              isSelected={filter === "events"}
+            />
+          </div>
         </div>
-        <div className="flex flex-row space-x-2 mt-2">
-          <BigButton Icon={MdEmojiPeople} text="Friends" onClick={() => {}} />
-          <BigButton Icon={StarIcon} text="Events" onClick={() => {}} />
+        <div className="flex flex-col flex-grow p-2">
+          {places.features.map((feature) => {
+            switch (filter) {
+              case "all":
+                return (
+                  <Event
+                    Icon={StarIcon}
+                    text={feature.properties.title}
+                    onClick={() => {}}
+                  />
+                );
+              case "events":
+                if (eventTypes.includes(feature.properties.type))
+                  return (
+                    <Event
+                      Icon={StarIcon}
+                      text={feature.properties.title}
+                      onClick={() => {}}
+                    />
+                  );
+                break;
+              case "friends":
+                if (!eventTypes.includes(feature.properties.type))
+                  return (
+                    <Event
+                      Icon={StarIcon}
+                      text={feature.properties.title}
+                      onClick={() => {}}
+                    />
+                  );
+            }
+          })}
         </div>
-      </div>
-      <div className="flex flex-col p-2">
-        <Event Icon={MdEmojiPeople} text="Bartosz" onClick={() => {}} />
-        <Event Icon={StarIcon} text="Tomasz Resto Bar" onClick={() => {}} />
-        <Event Icon={MdEmojiPeople} text="Szymon" onClick={() => {}} />
-        <Event Icon={MdEmojiPeople} text="Hubert" onClick={() => {}} />
-        <Event Icon={MdEmojiPeople} text="Pawel" onClick={() => {}} />
       </div>
     </div>
   );
