acec9a877146aa0b8ad17211f2da3f7f919ab3d1
Adrian Szlag <92638498+AdrianSzlag@users.noreply.github.com>
2023-03-06 04:55:35 +0000

    e

--- src/components/createEvent.tsx
+++ src/components/createEvent.tsx
@@ -1,16 +1,9 @@
-import React, { useState } from "react";
+import React from "react";
+
 interface Props {
-  hideCreateEventPopup: () => void;
+  isOpen: boolean;
 }
-export default function CreateEvent({ hideCreateEventPopup }: Props) {
-  return (
-    <div
-      className={` z-30 bg-black/70 right-0 bottom-0 fixed left-0 top-0 flex justify-center items-center`}
-      onClick={() => {
-        hideCreateEventPopup();
-      }}
-    >
-      <div className="bg-white rounded-sm p-4">asdfsadfasdf</div>
-    </div>
-  );
+
+export default function createEvent({ isOpen }: Props) {
+  return <div>createEvent</div>;
 }
---
+++ src/components/overview.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+interface Props {
+  title: string;
+  description: string;
+  image: string;
+}
+
+export default function overview() {
+  return <div> dsfa</div>;
+}
