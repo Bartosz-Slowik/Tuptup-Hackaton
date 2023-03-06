41c7f1d3d26e5c1a30bd9c0488e0db9d23934dba
SzSalamon <92257198+SzymonSalamon@users.noreply.github.com>
2023-03-06 07:05:48 +0000

    design changes

--- src/components/overview.tsx
+++ src/components/overview.tsx
@@ -25,12 +25,12 @@ export default function Overview({
       <div className="-mx-1 py-1">
         <img src={image} alt="" />
       </div>
-      <div className="flex flex-row justify-center font-bolder p-1 m-2 rounded bg-gray-200 cursor-pointer text-center">
+      <div className="flex flex-row justify-center font-bolder p-1 m-2 rounded bg-blue-600 cursor-pointer text-center text-white">
         JOIN
       </div>
       <div className="flex flex-col text-xs px-2 mb-2 text-center">
         <div className="font-bold ">Your friends are here:</div>
-        <div className="text-center">{friends}</div>
+        <div className="text-center text-blue-400">{friends}</div>
       </div>
     </div>
   );
