f8bdc32022c97b08d989543656d6ee48c2d215d5
Adrian Szlag <aszlag00@gmail.com>
2023-03-20 18:10:54 +0000

    spiner centered

--- frontend/src/components/UI/Spinner.tsx
+++ frontend/src/components/UI/Spinner.tsx
@@ -1,9 +1,9 @@
 const Spinner = () => {
   return (
-    <div className="translate-x-2/4 translate-y-2/4" role="status">
+    <div className="flex flex-row justify-center" role="status">
       <svg
         aria-hidden="true"
-        className="mr-2 h-8 w-8 animate-spin fill-blue-600 text-gray-200 dark:text-gray-600"
+        className="h-8 w-8 animate-spin fill-blue-600 text-gray-200 dark:text-gray-600 "
         viewBox="0 0 100 101"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
