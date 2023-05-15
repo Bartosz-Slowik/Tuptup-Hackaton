b8f1a46817e430f035efe6f559cb8f6d0f46c904
Adrian Szlag <aszlag00@gmail.com>
2023-05-15 12:34:04 +0000

    qf2

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -2,7 +2,7 @@ import { useNavigate } from "react-router-dom";
 import LoginForm from "../components/Login/LoginForm";
 import MyMap from "../components/Map/LoginMap";
 import { useEffect } from "react";
-import { getToken } from "../utils/auth";
+import { getToken, setToken } from "../utils/auth";
 
 const Login = () => {
   const navigate = useNavigate();
@@ -15,17 +15,16 @@ const Login = () => {
   }, []);
 
   const onSuccessHandler = () => {
+    setToken("token");
     navigate("/");
   };
 
   return (
     <div className="fixed bottom-0 left-0 right-0 top-0 flex items-center justify-center rounded-2xl">
-      <MyMap/>
-      <div className="border-black border-2 z-10 bg-white text-center p-4 font-serif">
-      <LoginForm onSuccess={onSuccessHandler} />
+      <MyMap />
+      <div className="z-10 border-2 border-black bg-white p-4 text-center font-serif">
+        <LoginForm onSuccess={onSuccessHandler} />
       </div>
-      
-
     </div>
   );
 };
